d3de43717d2be2e29b14f2623e281845f59a9ebc
Usuario <Usuario@10.235.93.195>
2018-12-06 15:15:23 -0600

    commitTarea

---
+++ TareaFinal/src/model/TextField.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model;
+
+import filemanager.ReaderFiletxt;
+import java.awt.Color;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Maria Rodriguez
+ */
+public class TextField extends JTextField {
+    ReaderFiletxt reader = new ReaderFiletxt();
+   
+    private int [][] datos;
+    
+
+    public void setColorWhite() {
+        setBackground(Color.white);
+        setEditable(true);
+    }
+    public void setColorBlack (){
+        setBackground(Color.black);
+        setEditable(false);
+    }
+  
+    
+    
+}
